7e045328d8d19e2d13119df5e08459b68c7562a4
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2024-05-01 13:18:49 +0300

    feature/modularization - user detail and call info modules added

---
+++ feature/calls/src/main/java/com/ahmetocak/calls/CallsScreen.kt
@@ -0,0 +1,8 @@
+package com.ahmetocak.calls
+
+import androidx.compose.runtime.Composable
+
+@Composable
+internal fun CallsRoute() {
+
+}
---
+++ feature/calls/src/main/java/com/ahmetocak/calls/CallsViewModel.kt
@@ -0,0 +1,9 @@
+package com.ahmetocak.calls
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CallsViewModel @Inject constructor(): ViewModel() {
+}
